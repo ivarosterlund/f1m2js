4a14677b95828b9491f48ae84b18707e20c1a157
Ivar Österlund <33647@ma-web.nl>
2022-01-12 13:57:34 +0100

    werkende koelkast

--- web/les2-boodschappen/script.js
+++ web/les2-boodschappen/script.js
@@ -1,1 +1,23 @@
 console.log("script is geladen");
+
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById("fruit");
+
+function zetInFruitLa(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2')
+    nieuwElement.innerHTML = fruit;
+    fruitvak.appendChild(nieuwElement);
+    nieuwElement.className = 'rood'; 
+
+}
+
+function zetInGroenteLa(groente){
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2')
+    nieuwElement.innerHTML = groente;
+    groentevak.appendChild(nieuwElement);
+    nieuwElement.className = 'groen';
+}
